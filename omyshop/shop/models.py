09e003db88909dc7a0da1214195d01c21efd6bb1
namwoo <garson1362@gmail.com>
2018-06-19 12:58:19 +0900

    Add get_absolute_url method for Category and Product model class

--- omyshop/shop/models.py
+++ omyshop/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -12,6 +13,10 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product-list-by-category',
+                       args=[self.slug])
 
 
 class Product(models.Model):
@@ -35,3 +40,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product-detail',
+                       args=[self.id, self.slug])
