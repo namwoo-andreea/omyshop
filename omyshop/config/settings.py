a4dc78d81b238b6f4d2f033b7b439c00f2157f03
namwoo <garson1362@gmail.com>
2018-06-19 13:33:21 +0900

    Set media and static files URL and dir path

--- omyshop/config/settings.py
+++ omyshop/config/settings.py
@@ -25,6 +25,8 @@ config_secret_common = json.loads(config_secret_common_str)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config_secret_common["django"]["SECRET_KEY"]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -126,3 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
