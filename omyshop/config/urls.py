10cfdd027b51dccc0b9c9d92e417cf7ed72ec474
namwoo <garson1362@gmail.com>
2018-06-22 09:59:18 +0900

    Create customer orders

--- omyshop/config/urls.py
+++ omyshop/config/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls', namespace='shop')),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
 ]
 
 if settings.DEBUG:
