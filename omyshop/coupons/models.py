c39b0d9e4ebea85fdbe912bc5cc1685bf8196587
namwoo <garson1362@gmail.com>
2018-06-26 11:51:41 +0900

    Create Coupon model class

---
+++ omyshop/coupons/models.py
@@ -0,0 +1,15 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+    discount = models.IntegerField(validators=[MinValueValidator(0),
+                                               MaxValueValidator(100)])
+    min_payment = models.DecimalField(max_digits=10, decimal_places=2)
+    enable = models.BooleanField()
+
+    def __str__(self):
+        return self.code
